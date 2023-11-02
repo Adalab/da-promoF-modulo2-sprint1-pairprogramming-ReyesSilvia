6c974ff477a392cca7b5f66915d7171dbf9f72b4
ReyesFP <referpe@gmail.com>
2023-11-02 11:30:05 +0100

    día 2

--- SQL/Tienda_zapatillas.sql
+++ SQL/Tienda_zapatillas.sql
@@ -49,3 +49,53 @@
     ON DELETE RESTRICT
     ON UPDATE RESTRICT
  );
+ 
+  ALTER TABLE `zapatillas`
+	ADD COLUMN `marca` VARCHAR (45) NOT NULL, 
+    ADD COLUMN `talla` SMALLINT NOT NULL; 
+
+ALTER TABLE `empleados`
+	MODIFY COLUMN `salario` FLOAT NOT NULL;
+
+ALTER TABLE `clientes`
+	DROP COLUMN `pais`; 
+    
+ALTER TABLE `facturas`
+	ADD COLUMN `total` FLOAT; 
+
+ALTER TABLE `empleados`
+	DROP COLUMN `salarios`;
+    
+INSERT INTO `zapatillas` (`modelo`, `color`, `marca`, `talla`)
+	VALUES ('XQYUN', 'Negro', 'Nike', 42),('UOPMN', 'Rosas', 'Nike', 39),('OPNYT', 'Verdes', 'Adidas', '35');
+    
+INSERT INTO `empleados` (`nombre`, `tienda`, `salario`, `fecha_incorporacion`)
+	VALUES ('Laura', 'Alcobendas',25987, '2010-09-03'), ('Maria', 'Sevilla', 0, '2001-04-11'), ('Esther', 'Oviedo', 30165.98, '2000-11-29'); 
+
+ALTER TABLE `clientes`
+	MODIFY COLUMN `numero_telefono` VARCHAR(9);
+ 
+ INSERT INTO `clientes` (`nombre`, `numero_telefono`, `email`, `direccion`, `ciudad`, `provincia`, `codigo_postal`)
+	VALUES ('Monica', '123456789','monica@email.com', 'Calle Felicidad', 'Mostoles', 'Madrid', 28176), 
+    ('Lorena', '289345678','lorena@email.com', 'Calle Alegria', 'Barcelona', 'Barcelona', 12346), 
+    ('Carmen', '298463759','carmen@email.com', 'Calle del Color', 'Vigo', 'Pontevedra', 23456);    
+
+INSERT INTO `facturas`(`numero_factura`, `fecha`, `id_zapatilla`, `id_empleado`, `id_cliente`, `total`)
+	VALUES ('123', '2001-12-11', 1,2,1,54.98), ('1234', '2005-05-23', 1,1,3,89.91), ('12345', '2015-09-18', 2,3,3,76.23);
+    
+UPDATE `zapatillas`
+	SET `color` = 'Amarillas'
+    WHERE `id_zapatilla` = 2;
+
+UPDATE `empleados`
+	SET `tienda`= 'A Coruña'
+    WHERE `id_empleado`=1;
+    
+UPDATE `clientes`
+	SET `numero_telefono`= '123456728'
+    WHERE `id_cliente` =1; 
+    
+UPDATE `facturas`
+	SET `total`=89.91
+    WHERE `id_zapatilla`=2;
+    
